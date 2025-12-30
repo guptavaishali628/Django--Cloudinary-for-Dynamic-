0234bb219eb6f3ba4c043d411d167dcf2c2ba5ec
vaishali <guptavaishali628@gmail.com>
2025-12-30 10:23:00 +0530

    dynamic cloudinary

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
+def index(req):
+    return render(req,'index.html')
+
+def register(req):
+    if req.method=='POST':
+        image=req.POST.get('image')
+        video=req.POST.get('video')
+        audio=req.POST.get('audio')
+        file=req.POST.get('file')
+        Student.objects.create(Image=image, Video=video, Audio=audio, File=file).save()
+        return render(req,'show_data.html')
+
+def show_data(req):
+    data=Student.objects.all()
+    return render(req,'show_data.html', {'data':data})   
+    
+
+
+
+
--- project/project/urls.py
+++ project/project/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index, name='index'),
+    path('register/',register,name='register'),
+    path('show_data/',show_data,name='show_data')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
